2eee9f64804754b655965daac544188c40deee1c
WRWPhillips <42WPhillips42@gmail.com>
2022-08-04 11:50:46 -0500

    Main entrypoint along with Snack class created
    -- Next I need to make snack machines

---
+++ src/snackbar/Main.java
@@ -0,0 +1,10 @@
+package snackbar;
+
+public class Main {
+	public static void main(String[] args) {
+		Snack snack1 = new Snack("Peanut Mufaletta", 6, 15);
+		int snackId = snack1.getId();
+	        System.out.println(snackId);
+	}
+}
+
---
+++ src/snackbar/Snack.java
@@ -0,0 +1,89 @@
+package snackbar;
+
+public class Snack
+{
+	private static int maxId = 0;
+	private int id;
+	private String name; 
+	private int quantity = 0;
+	private int cost = 0;
+	private static int vendingMachineId = 1;
+	private int total;
+
+	//Constructor
+	public Snack(String name, int quantity, int cost)
+	{	
+		maxId++;
+		id = maxId;
+		this.name = name;
+		this.quantity = quantity;
+		this.cost = cost;
+		this.vendingMachineId = vendingMachineId;
+		this.total = quantity * cost;
+	}
+
+	//Getters
+	public int getId() {
+		return this.id;
+	}
+
+	public int getMaxId() {
+		return this.maxId;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getQuantity() {
+		return this.quantity;
+	}
+
+	public double getCost() {
+		return this.cost;
+	}
+
+	public int getVendingMachineId() {
+		return this.vendingMachineId;
+	}
+
+	public int getTotal() {
+		return this.total;
+	}
+
+	//Setters
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public void setVendingMachineId(int vendingMachineId) {
+		this.vendingMachineId = vendingMachineId;
+	}
+
+	public void setMaxId(int id) {
+		this.maxId = maxId;
+	}
+
+	public void setTotal() {
+		this.total = total;
+	}
+
+	@Override
+	public String toString() {
+		String rtnStr = 
+		"Id: " + id + "\n Name: " + name + "\n Quantity: " + quantity + "\n Cost: " + cost + "\n VendingMachineId: " + vendingMachineId + "\n Total: " + total + "\n________________";
+		return rtnStr;
+	};
+}
